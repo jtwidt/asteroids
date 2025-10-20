3f0e7c2afaffb6b85ec1a51151587162f8fc20ff
Jeremy Twidt <jtwidt@gmail.com>
2025-10-20 00:11:51 -0500

    added split function

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if shot.collision(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         for entity in drawable:
             entity.draw(screen)
